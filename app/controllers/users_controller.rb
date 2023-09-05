13f4455e0ed4e1b4f58e3b1e78e613fdef4a8f04
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-09-05 16:57:50 -0400

    Add user edit/update view + controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email)
+    params.require(:user).permit(:id, :username, :password, :email)
   end
 
 end
